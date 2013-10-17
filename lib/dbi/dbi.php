df865740c66cd48c4d977dcb9764aa8badc03c90
Fabio Trezzi <fabio.trezzi@gmail.com>
2013-10-17 15:29:19 +0200

    Introduced a new blank line between @param and @return

--- lib/dbi/dbi.php
+++ lib/dbi/dbi.php
@@ -54,6 +54,7 @@ class Ai1ec_Dbi
 	 * Perform a MySQL database query, using current database connection.
 	 *
 	 * @param string $query Database query
+	 *
 	 * @return int|false Number of rows affected/selected or false on error
 	 */
 	public function query( $sql_query ) {
@@ -69,6 +70,7 @@ class Ai1ec_Dbi
 	 *	 *
 	 * @param string|null $query Optional. SQL query. Defaults to previous query.
 	 * @param int $x Optional. Column to return. Indexed from 0.
+	 *
 	 * @return array Database query result. Array indexed from 0 by SQL result row number.
 	 */
 	public function get_col( $query = null , $x = 0 ) {
@@ -101,6 +103,7 @@ class Ai1ec_Dbi
 	 * 	being called like {@link http://php.net/sprintf sprintf()}.
 	 * @param mixed $args,... further variables to substitute into the query's placeholders if being called like
 	 * 	{@link http://php.net/sprintf sprintf()}.
+	 *
 	 * @return null|false|string Sanitized query string, null if there is no query, false if there is an error and string
 	 * 	if there was something to prepare
 	 */
@@ -133,6 +136,7 @@ class Ai1ec_Dbi
 	 * @param string $output Optional. Any of ARRAY_A | ARRAY_N | OBJECT | OBJECT_K constants. With one of the first three, return an array of rows indexed from 0 by SQL result row number.
 	 * 	Each row is an associative array (column => value, ...), a numerically indexed array (0 => value, ...), or an object. ( ->column = value ), respectively.
 	 * 	With OBJECT_K, return an associative array of row objects keyed by the value of each row's first column's value. Duplicate keys are discarded.
+	 *
 	 * @return mixed Database query results
 	 */
 	public function get_results( $query, $output  )
@@ -149,6 +153,7 @@ class Ai1ec_Dbi
 	 * @param string|null $query Optional. SQL query. Defaults to null, use the result from the previous query.
 	 * @param int $x Optional. Column of value to return. Indexed from 0.
 	 * @param int $y Optional. Row of value to return. Indexed from 0.
+	 *
 	 * @return string|null Database query result (as string), or null on failure
 	 */
 	public function get_var( $query = null, $x = 0, $y = 0 ) {
@@ -164,6 +169,7 @@ class Ai1ec_Dbi
 	 * @param string $output Optional. one of ARRAY_A | ARRAY_N | OBJECT constants. Return an associative array (column => value, ...),
 	 * 	a numerically indexed array (0 => value, ...) or an object ( ->column = value ), respectively.
 	 * @param int $y Optional. Row to return. Indexed from 0.
+	 *
 	 * @return mixed Database query result in format specified by $output or null on failure
 	 */
 	public function get_row( $query = null, $output = OBJECT, $y = 0 ) {
@@ -177,6 +183,7 @@ class Ai1ec_Dbi
 	 * @param array $data Data to insert (in column => value pairs). Both $data columns and $data values should be "raw" (neither should be SQL escaped).
 	 * @param array|string $format Optional. An array of formats to be mapped to each of the value in $data. If string, that format will be used for all of the values in $data.
 	 * 	A format is one of '%d', '%f', '%s' (integer, float, string). If omitted, all values in $data will be treated as strings unless otherwise specified in wpdb::$field_types.
+	 *
 	 * @return int|false The number of rows inserted, or false on error.
 	 */
 	public function insert( $table, $data, $format = null ) {
@@ -192,6 +199,7 @@ class Ai1ec_Dbi
 	 * @param array|string $format Optional. An array of formats to be mapped to each of the values in $data. If string, that format will be used for all of the values in $data.
 	 * 	A format is one of '%d', '%f', '%s' (integer, float, string). If omitted, all values in $data will be treated as strings unless otherwise specified in wpdb::$field_types.
 	 * @param array|string $where_format Optional. An array of formats to be mapped to each of the values in $where. If string, that format will be used for all of the items in $where. A format is one of '%d', '%f', '%s' (integer, float, string). If omitted, all values in $where will be treated as strings.
+	 *
 	 * @return int|false The number of rows updated, or false on error.
 	 */
 	public function update( $table, $data, $where, $format = null, $where_format = null ) {
@@ -209,6 +217,7 @@ class Ai1ec_Dbi
 
 	/**
 	* @param string $table table name
+	*
 	* @return string the full table name for the requested table
 	*/
 	public function get_table_name( $table ) {
@@ -222,6 +231,7 @@ class Ai1ec_Dbi
 	 * Escapes content by reference for insertion into the database, for security
 	 *
 	 * @param string $string to escape
+	 *
 	 * @return void
 	 */
 	function escape_by_ref( &$string ) {
@@ -234,6 +244,7 @@ class Ai1ec_Dbi
 	 * Real escape, using mysql_real_escape_string()
 	 *
 	 * @param  string $string to escape
+	 *
 	 * @return string escaped
 	 */
 	function _real_escape( $string ) {

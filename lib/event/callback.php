86cda12f8bd1d73236046ff44a30f6cc07a4d61a
Borislav Angelov <borislav.angelov@gmail.com>
2013-11-16 02:30:25 +0200

    Code style changes and adding doc comments

--- lib/event/callback.php
+++ lib/event/callback.php
@@ -1,17 +1,64 @@
 <?php
 
-class Ai1ec_Event_Callback {    
+/**
+ * Event Callback creation.
+ *
+ * @author     Time.ly Network Inc.
+ * @since      2.0
+ * 
+ * @instantiator new
+ * @package      AI1EC
+ * @subpackage   AI1EC.Event
+ */
+class Ai1ec_Event_Callback {
+    
+	/**
+	 * @var Ai1ec_Object_Registry The Object registry.
+	 */    
     protected $_registry      = null;
+    
+
+	/**
+	 * @var string The registry method name defined in the class map.
+	 */    
     protected $_registry_name = null;    
+    
+	/**
+	 * @var string The method invoked by the current callback.
+	 */    
     protected $_method        = null;
     
-    public function __construct( Ai1ec_Object_Registry $registry, $path, $method ) {
+	/**
+	 * Initiate callback objects.
+	 *
+	 * @param Ai1ec_Object_Registry $registry Registry object.
+     * @param string                $path     Registry method name defined in the class map.
+     * @param string                $method   Method invoked by the currect callback.
+	 *
+	 * @return void Constructor does not return.
+	 */    
+    public function __construct( 
+        Ai1ec_Object_Registry $registry, 
+        $path, 
+        $method 
+    ) {
         $this->_registry      = $registry;
         $this->_registry_name = $path;
         $this->_method        = $method;
     }
 
+	/**
+	 * Invoke the method added to the current callback.
+	 *
+	 * @param array $argv Multiple arguments applied to the method.
+	 *
+	 * @return mixed Value returned by the current method.
+	 */    
     public function execute( array $argv ) {
-        return $this->_registry->dispatch( $this->_registry_name, $this->_method, $argv );
+        return $this->_registry->dispatch( 
+            $this->_registry_name, 
+            $this->_method, 
+            $argv 
+        );
     }
 }
--- lib/event/dispatcher.php
+++ lib/event/dispatcher.php
@@ -1,16 +1,55 @@
 <?php
 
+/**
+ * Event Dispatcher processing.
+ *
+ * @author     Time.ly Network Inc.
+ * @since      2.0
+ * 
+ * @package      AI1EC
+ * @subpackage   AI1EC.Event
+ */
 class Ai1ec_Event_Dispatcher {
-    public function register( $hook, Ai1ec_Event_Callback $entity, $priority = 10, $accepted_args = 1 ) {
+    
+	/**
+	 * Register callback object.
+	 *
+     * @param string               $hook          Name of the event hook.
+	 * @param Ai1ec_Event_Callback $entity        Event Callback object.
+     * @param integer              $priority      Priorify of the event hook execution.
+     * @param integer              $accepted_args Number of accepted method parameters.
+	 *
+	 * @return Ai1ec_Event_Dispatcher Event Dispatcher Object.
+	 */    
+    public function register( 
+        $hook, 
+        Ai1ec_Event_Callback $entity, 
+        $priority = 10, 
+        $accepted_args = 1 
+    ) {
         if ( !isset( $this->_registered[$hook] ) ) {
             $this->_registered[$hook] = array();
         }
         
-        $this->_registered[$hook][] = compact( 'entity', 'priority', 'accepted_args' );
-        add_action( $hook, array( $this, 'execute' ), $priority, $accepted_args );
+        $this->_registered[$hook][] = compact( 
+            'entity', 
+            'priority', 
+            'accepted_args' 
+        );
+        add_action( 
+            $hook, 
+            array( $this, 'execute' ), 
+            $priority, 
+            $accepted_args 
+        );        
         return $this;
     }
 
+	/**
+	 * Execute current action/filter event hook.
+	 *
+	 * @return mixed Value returned by the current action/filter event hook.
+	 */
     public function execute() {
         $hook = current_filter();
         if ( !isset( $this->_registered[$hook] ) ) {

dad1f009bb3ad62200125dafca87c635fa6d686d
Justas Butkus <justas@butkus.lt>
2013-11-18 20:56:21 +0200

    AIOEC-1278 #comment Fixed whitespace usage. #time 15m

--- lib/event/callback/abstract.php
+++ lib/event/callback/abstract.php
@@ -3,10 +3,10 @@
 /**
  * Abstract Class for Callback Events.
  *
- * @author     Time.ly Network Inc.
- * @since      2.0
+ * @author	   Time.ly Network Inc.
+ * @since	   2.0
  *
- * @package    AI1EC
+ * @package	   AI1EC
  * @subpackage AI1EC.Event
  */
 abstract class Ai1ec_Event_Callback_Abstract {
@@ -14,47 +14,48 @@ abstract class Ai1ec_Event_Callback_Abstract {
 	/**
 	 * @var Ai1ec_Object_Registry The Object registry.
 	 */
-    protected $_registry      = null;
+	protected $_registry	  = null;
 
 	/**
 	 * @var string The registry method name defined in the class map.
 	 */
-    protected $_registry_name = null;
+	protected $_registry_name = null;
 
 	/**
 	 * @var string The method invoked by the current callback.
 	 */
-    protected $_method        = null;
+	protected $_method		  = null;
 
 	/**
 	 * Initiate callback objects.
 	 *
 	 * @param Ai1ec_Object_Registry $registry Registry object.
-     * @param string                $path     Registry method name defined in the class map.
-     * @param string                $method   Method invoked by the currect callback.
+	 * @param string				$path	  Registry method name defined in the class map.
+	 * @param string				$method	  Method invoked by the currect callback.
 	 *
 	 * @return void Constructor does not return.
 	 */
-    public function __construct(
-        Ai1ec_Object_Registry $registry,
-        $path,
-        $method
-    ) {
-        $this->_registry      = $registry;
-        $this->_registry_name = $path;
-        $this->_method        = $method;
-    }
+	public function __construct(
+		Ai1ec_Object_Registry $registry,
+		$path,
+		$method
+	) {
+		$this->_registry	  = $registry;
+		$this->_registry_name = $path;
+		$this->_method		  = $method;
+	}
 
 	/**
 	 * Invoke the method added to the current callback.
 	 *
 	 * @return mixed Value returned by the current method.
 	 */
-    public function run() {
-        return $this->_registry->dispatch(
-            $this->_registry_name,
-            $this->_method,
-            func_get_args()
-        );
-    }
+	public function run() {
+		return $this->_registry->dispatch(
+			$this->_registry_name,
+			$this->_method,
+			func_get_args()
+		);
+	}
+
 }
--- lib/event/dispatcher.php
+++ lib/event/dispatcher.php
@@ -6,7 +6,7 @@
  * @author     Time.ly Network Inc.
  * @since      2.0
  *
- * @package    AI1EC
+ * @package	   AI1EC
  * @subpackage AI1EC.Event
  */
 class Ai1ec_Event_Dispatcher {
@@ -14,35 +14,31 @@ class Ai1ec_Event_Dispatcher {
 	/**
 	 * Register callback object.
 	 *
-     * @param string                        $hook          Name of the event hook.
-	 * @param Ai1ec_Event_Callback_Abstract $entity        Event Callback object.
-     * @param integer                       $priority      Priorify of the event hook execution.
-     * @param integer                       $accepted_args Number of accepted method parameters.
+	 * @param string						$hook		   Name of the event hook.
+	 * @param Ai1ec_Event_Callback_Abstract $entity		   Event Callback object.
+	 * @param integer						$priority	   Priorify of the event hook execution.
+	 * @param integer						$accepted_args Number of accepted method parameters.
 	 *
 	 * @return Ai1ec_Event_Dispatcher Event Dispatcher Object.
 	 */
-    public function register(
-        $hook,
-        Ai1ec_Event_Callback_Abstract $entity,
-        $priority = 10,
-        $accepted_args = 1
-    ) {
-        if ($entity instanceof Ai1ec_Event_Callback_Filter) {
-            add_filter(
-                $hook,
-                array( $entity, 'run' ),
-                $priority,
-                $accepted_args
-            );
-        } else {
-            add_action(
-                $hook,
-                array( $entity, 'run' ),
-                $priority,
-                $accepted_args
-            );
-        }
+	public function register(
+		$hook,
+		Ai1ec_Event_Callback_Abstract $entity,
+		$priority      = 10,
+		$accepted_args = 1
+	) {
+		$wp_method = 'add_action';
+		if ( $entity instanceof Ai1ec_Event_Callback_Filter ) {
+			$wp_method = 'add_filter';
+		}
+		$wp_method(
+			$hook,
+			array( $entity, 'run' ),
+			$priority,
+			$accepted_args
+		);
 
-        return $this;
-    }
+		return $this;
+	}
+
 }

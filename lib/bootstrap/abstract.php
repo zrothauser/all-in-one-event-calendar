e79de996fbeb054a75be1a65d1b6899066ab4ca3
Justas Butkus <justas@butkus.lt>
2013-11-22 19:48:31 +0200

    Make Ai1ec_Base abstract.

--- lib/bootstrap/abstract.php
+++ lib/bootstrap/abstract.php
@@ -8,7 +8,7 @@
  * @package    Ai1EC
  * @subpackage Ai1EC.Bootstrap
  */
-class Ai1ec_Base {
+abstract class Ai1ec_Base {
 
 	/**
 	 * @var Ai1ec_Registry_Object
@@ -18,9 +18,12 @@ class Ai1ec_Base {
 	/**
 	 * The contructor method.
 	 *
-	 * @param Ai1ec_Registry_Object $registry
+	 * Stores in object injected registry object.
+	 *
+	 * @param Ai1ec_Registry_Object $registry Injected registry object.
 	 */
 	public function __construct( Ai1ec_Registry_Object $registry ) {
 		$this->_registry = $registry;
 	}
+
 }

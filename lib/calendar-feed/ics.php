2623f8b403dc8515a0557f9d19509a3a64caa2eb
Nicola Peluchetti <nicola.peluchetti@gmail.com>
2013-11-26 15:16:46 +0100

    Fixed errors in calendare feeds page

--- lib/calendar-feed/ics.php
+++ lib/calendar-feed/ics.php
@@ -354,59 +354,63 @@ class Ai1ecIcsConnectorPlugin extends Ai1ec_Connector_Plugin {
 			$feed_urls[] = $db->escape( $feed->feed_url );
 		}
 		unset( $feed );
-		$query = 'SELECT `ical_feed_url`, COUNT( `post_id` ) AS `imported`' .
-			' FROM ' . $db->get_table_name( 'ai1ec_events' ) .
-			' WHERE `ical_feed_url` IN (\'' .
-			implode( '\', \'', $feed_urls ) .
-			') GROUP BY `ical_feed_url`';
-		unset( $feed_urls );
-		$counters = array();
-		$count_result = $db->get_results( $query );
-		foreach ( $count_result as $row ) {
-			$counters[$row->ical_feed_url] = (int)$row->imported;
-		}
-		unset( $count_result );
 		$html = '';
-		foreach ( $rows as $row ) {
-			$events          = 0;
-			$feed_categories = explode( ',', $row->feed_category );
-			$categories      = array();
-			if ( isset( $counters[$row->feed_url] ) ) {
-				$events = $counters[$row->feed_url];
+		if ( ! empty( $feed_urls ) ) {
+			$query = 'SELECT `ical_feed_url`, COUNT( `post_id` ) AS `imported`' .
+				' FROM ' . $db->get_table_name( 'ai1ec_events' ) .
+				' WHERE `ical_feed_url` IN (\'' .
+				implode( '\', \'', $feed_urls ) .
+				'\') GROUP BY `ical_feed_url`';
+			unset( $feed_urls );
+			$counters = array();
+			$count_result = $db->get_results( $query );
+			foreach ( $count_result as $row ) {
+				$counters[$row->ical_feed_url] = (int)$row->imported;
 			}
+			unset( $count_result );
 
-			foreach ( $feed_categories as $cat_id ) {
-				$feed_category = get_term(
-					$cat_id,
-					'events_categories'
-				);
-				if ( $feed_category && ! is_wp_error( $feed_category ) ) {
-					$categories[] = $feed_category->name;
+			foreach ( $rows as $row ) {
+				$events          = 0;
+				$feed_categories = explode( ',', $row->feed_category );
+				$categories      = array();
+				if ( isset( $counters[$row->feed_url] ) ) {
+					$events = $counters[$row->feed_url];
 				}
-			}
 
-			$args          = array(
-				'feed_url'            => $row->feed_url,
-				'event_category'      => implode( ', ', $categories ),
-				'tags'                => stripslashes(
-					str_replace( ',', ', ', $row->feed_tags )
-				),
-				'feed_id'             => $row->feed_id,
-				'comments_enabled'    => (bool) intval(
-					$row->comments_enabled
-				),
-				'map_display_enabled' => (bool) intval(
-					$row->map_display_enabled
-				),
-				'keep_tags_categories' => (bool) intval(
-					$row->keep_tags_categories
-				),
-				'events'              => $events,
-			);
-			$loader = $this->_registry->get( 'theme.loader' );
-			$file = $loader->get_file( 'feed_row.php', $args, true );
-			$html .= $file->get_content();
+				foreach ( $feed_categories as $cat_id ) {
+					$feed_category = get_term(
+						$cat_id,
+						'events_categories'
+					);
+					if ( $feed_category && ! is_wp_error( $feed_category ) ) {
+						$categories[] = $feed_category->name;
+					}
+				}
+
+				$args          = array(
+					'feed_url'            => $row->feed_url,
+					'event_category'      => implode( ', ', $categories ),
+					'tags'                => stripslashes(
+						str_replace( ',', ', ', $row->feed_tags )
+					),
+					'feed_id'             => $row->feed_id,
+					'comments_enabled'    => (bool) intval(
+							$row->comments_enabled
+						),
+					'map_display_enabled' => (bool) intval(
+							$row->map_display_enabled
+						),
+					'keep_tags_categories' => (bool) intval(
+							$row->keep_tags_categories
+						),
+					'events'              => $events,
+				);
+				$loader = $this->_registry->get( 'theme.loader' );
+				$file = $loader->get_file( 'feed_row.php', $args, true );
+				$html .= $file->get_content();
+			}
 		}
+
 
 		return $html;
 	}

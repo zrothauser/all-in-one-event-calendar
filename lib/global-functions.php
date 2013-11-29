ef33bcd46be3133b7baa7d2672ca24db7daa66a0
borislav-angelov <borislav.angelov@gmail.com>
2013-11-29 16:51:01 +0200

    Define global functions

---
+++ lib/global-functions.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Define global functions
+ *
+ * @author     Time.ly Network Inc.
+ * @since      2.0
+ *
+ * @package    AI1EC
+ * @subpackage AI1EC.Lib
+ */
+
+function ai1ec_return_false() {
+	return false;
+}

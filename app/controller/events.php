17d12552580cb725694aa8ed196a32edc5eaf08b
Borislav Angelov <borislav.angelov@gmail.com>
2013-12-02 22:53:28 +0200

    AIOEC-1294 Migrate excerpt template

--- app/controller/events.php
+++ app/controller/events.php
@@ -43,4 +43,82 @@ class Ai1ec_Events_Controller extends Ai1ec_Base {
 		return $template->get_content();
 	}
 
+	/**
+	 * event_excerpt function
+	 *
+	 * Overrides what wp_trim_excerpt() returned if the post is an event,
+	 * and outputs better rich-text (but not too rich) excerpt instead.
+	 *
+	 * @param string $content Event content.
+	 *
+	 * @return void
+	 **/
+	public function event_excerpt( $content ) {
+		if ( get_post_type() != AI1EC_POST_TYPE ) {
+			return $content;
+		}
+
+		$event = new Ai1ec_Event( $this->_registry, get_the_ID() );
+
+		ob_start();
+
+		$this->excerpt_view( $event );
+
+		// Re-apply any filters to the post content that normally would have been
+		// applied if it weren't for our interference (below).
+		echo shortcode_unautop( wpautop(
+				$this->_registry->get( 'Ai1ec_Event_Helper' )->trim_excerpt(
+					apply_filters( 'the_content', $event->post->post_content )
+				)
+		) );
+
+		$page_content = ob_get_contents();
+		ob_end_clean();
+
+		return $page_content;
+	}
+
+	/**
+	 * event_excerpt_noautop function
+	 *
+	 * Conditionally apply wpautop() filter to content, only if it is not an
+	 * event.
+	 *
+	 * @param string $content Event content.
+	 *
+	 * @return void
+	 **/
+	public function event_excerpt_noautop( $content ) {
+		if ( get_post_type() != AI1EC_POST_TYPE ) {
+			return wpautop( $content );
+		}
+		return $content;
+	}
+
+	/**
+	 * Outputs event-specific details as HTML to be prepended to post content
+	 * when displayed in an excerpt format.
+	 *
+	 * @param Ai1ec_Event $event  The event being displayed
+	 *
+	 * @return void
+	 */
+	public function excerpt_view( $event ) {
+		$location = esc_html(
+			str_replace( "\n", ', ', rtrim( $event->get_location() ) )
+		);
+
+		$args = array(
+			'timespan_html' => $event->get_timespan_html(),
+			'location'      => $location,
+		);
+
+		$template = $this->_registry->get( 'theme.loader' )->get_file(
+			'twig/event-excerpt.twig',
+			$args,
+			false
+		);
+
+		echo $template->get_content();
+	}
 }

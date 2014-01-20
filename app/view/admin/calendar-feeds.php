5564a95e602518945722bd24ce5d84c8b5130baf
Justas Butkus <justas@butkus.lt>
2014-01-20 20:47:57 +0200

    FBCAL-2 #comment Fix code style. #time 10m

--- app/view/admin/calendar-feeds.php
+++ app/view/admin/calendar-feeds.php
@@ -12,8 +12,6 @@
 class Ai1ec_View_Calendar_Feeds extends Ai1ec_View_Admin_Abstract {
 
 	/**
-	 * admin_menu function
-	 *
 	 * Adds page and metabox to the menu.
 	 *
 	 * @wp-hook admin_menu
@@ -27,8 +25,8 @@ class Ai1ec_View_Calendar_Feeds extends Ai1ec_View_Admin_Abstract {
 		// =======================
 		$calendar_feeds = add_submenu_page(
 			AI1EC_ADMIN_BASE_URL,
-			__( 'Calendar Feeds', AI1EC_PLUGIN_NAME ),
-			__( 'Calendar Feeds', AI1EC_PLUGIN_NAME ),
+			Ai1ec_I18n::__( 'Calendar Feeds' ),
+			Ai1ec_I18n::__( 'Calendar Feeds' ),
 			'manage_ai1ec_feeds',
 			AI1EC_PLUGIN_NAME . '-feeds',
 			array( $this, 'display_page' )
@@ -52,8 +50,8 @@ class Ai1ec_View_Calendar_Feeds extends Ai1ec_View_Admin_Abstract {
 	 */
 	public function display_page() {
 		$settings = $this->_registry->get( 'model.settings' );
-		$loader = $this->_registry->get( 'theme.loader' );
-		$args = array(
+		$loader   = $this->_registry->get( 'theme.loader' );
+		$args     = array(
 			'title'             => __(
 				'All-in-One Event Calendar: Calendar Feeds',
 				AI1EC_PLUGIN_NAME
@@ -61,7 +59,7 @@ class Ai1ec_View_Calendar_Feeds extends Ai1ec_View_Admin_Abstract {
 			'settings_page'     => $settings->get( 'feeds_page' ),
 			'calendar_settings' => false,
 		);
-		$file = $loader->get_file( 'settings.php', $args, true );
+		$file     = $loader->get_file( 'settings.php', $args, true );
 		$file->render();
 	}
 
@@ -73,14 +71,14 @@ class Ai1ec_View_Calendar_Feeds extends Ai1ec_View_Admin_Abstract {
 	public function display_meta_box( $object, $box ) {
 		// register the calendar feeds page.
 		$calendar_feeds = $this->_registry->get( 'controller.calendar-feeds' );
-		$feeds = array( $this->_registry->get( 'calendar-feed.ics' ) );
-		$feeds = apply_filters( 'ai1ec_calendar_feeds', $feeds );
+		$feeds          = array( $this->_registry->get( 'calendar-feed.ics' ) );
+		$feeds          = apply_filters( 'ai1ec_calendar_feeds', $feeds );
 		foreach ( $feeds as $feed ) {
 			$calendar_feeds->add_plugin( $feed );
 		}
 		$calendar_feeds->handle_feeds_page_post();
 		$loader = $this->_registry->get( 'theme.loader' );
-		$file = $loader->get_file(
+		$file   = $loader->get_file(
 			'box_feeds.php',
 			array( 'calendar_feeds' => $calendar_feeds ),
 			true

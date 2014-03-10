54c665f55871b7542b687471cffc213c568c77a5
Justas Butkus <justas@butkus.lt>
2014-03-10 22:31:29 +0200

    Updating revision from "2.0.0-rc1/r031021" to "2.0.0-rc1/r031022".

--- all-in-one-event-calendar.php
+++ all-in-one-event-calendar.php
@@ -5,7 +5,7 @@
  * Description: A calendar system with month, week, day, agenda views, upcoming events widget, color-coded categories, recurrence, and import/export of .ics feeds.
  * Author: Time.ly Network Inc.
  * Author URI: http://time.ly/
- * Version: 2.0.0-rc1/r031021
+ * Version: 2.0.0-rc1/r031022
  * Text Domain: all-in-one-event-calendar
  * Domain Path: /language
  */

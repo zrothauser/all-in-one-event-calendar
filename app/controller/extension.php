d5ed6872a2c0069635a45eaa2f108610de53b13e
Nicola Peluchetti <nicola.peluchetti@gmail.com>
2014-05-12 16:24:49 +0200

    AIOEC-1752 add basic compatibility check

--- app/controller/extension.php
+++ app/controller/extension.php
@@ -10,7 +10,7 @@
  * @subpackage AI1EC.Controller
  */
 abstract class Ai1ec_Base_Extension_Controller {
-	
+
 	/**
 	 * @var Ai1ec_Registry_Object
 	 */
@@ -70,6 +70,24 @@ abstract class Ai1ec_Base_Extension_Controller {
 		Ai1ec_Event_Dispatcher $dispatcher
 	);
 
+	/**
+	 * Perform the basic compatibility check. 
+	 * 
+	 * @param string $ai1ec_version
+	 * 
+	 * @return boolean
+	 */
+	public function check_compatibility( $ai1ec_version ) {
+		return version_compare(
+			$this->minimum_core_required(), 
+			$ai1ec_version,
+			'>='
+		);
+	}
+
+	public function minimum_core_required() {
+		return '2.0.7';
+	}
 	/**
 	 * Removes options when uninstalling the plugin.
 	 */

a987643d6d95b37a52e560acbcc6ebe7a565be12
Farmer Paul <read.ishmael@gmail.com>
2014-07-01 11:48:53 -0400

    AIOEC-1503: Small improvement to validation message [no review required] #time 5m

--- app/controller/javascript.php
+++ app/controller/javascript.php
@@ -407,10 +407,10 @@ class Ai1ec_Javascript_Controller {
 				'When the "Input coordinates" checkbox is checked, "Longitude" is a required field.'
 			),
 			'ai1ec_contact_url_not_valid'         => Ai1ec_I18n::__(
-				'The URL you have entered in the Organizer Contact Info &gt; External URL seems to be invalid.'
+				'The URL you have entered in the <b>Organizer Contact Info</b> &gt; <b>External URL</b> seems to be invalid.'
 			),
 			'ai1ec_ticket_url_not_valid'           => Ai1ec_I18n::__(
-				'The URL you have entered in the Event Cost and Tickets &gt; Buy Tickets URL seems to be invalid.'
+				'The URL you have entered in the <b>Event Cost and Tickets</b> &gt; <b>Buy Tickets URL</b> seems to be invalid.'
 			),
 			'general_url_not_valid'          => Ai1ec_I18n::__(
 				'Please remember that URLs must start with either "http://" or "https://".'

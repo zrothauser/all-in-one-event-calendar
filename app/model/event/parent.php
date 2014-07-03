87debfa1a7444149709945bcddb679d654730f42
lukaspawlik <lukas@time.ly>
2014-07-03 19:55:55 +0200

    Handling first occurence editing.
    AIOEC-1809

--- app/model/event/parent.php
+++ app/model/event/parent.php
@@ -8,7 +8,7 @@
  * @subpackage   Ai1EC.Model
  */
 class Ai1ec_Event_Parent extends Ai1ec_Base {
-	
+
 	/**
 	 * event_parent method
 	 *
@@ -37,7 +37,7 @@ class Ai1ec_Event_Parent extends Ai1ec_Base {
 		) );
 		return add_post_meta( $event_id, $meta_key, $meta_value, true );
 	}
-	
+
 	/**
 	 * Get parent ID for given event
 	 *
@@ -75,7 +75,7 @@ class Ai1ec_Event_Parent extends Ai1ec_Base {
 		}
 		return $parent_id;
 	}
-	
+
 	/**
 	 * Returns a list of modified (children) event objects
 	 *
@@ -110,7 +110,7 @@ class Ai1ec_Event_Parent extends Ai1ec_Base {
 		}
 		return $objects;
 	}
-	
+
 	/**
 	 * admin_init_post method
 	 *
@@ -126,13 +126,21 @@ class Ai1ec_Event_Parent extends Ai1ec_Base {
 		) {
 			$old_post_id = $_POST['post_ID'];
 			$instance_id = $_POST['ai1ec_instance_id'];
+			$v = $this->_count_next_instances( $old_post_id, $instance_id );
+			if (
+				0 === $v
+			) {
+				return;
+			}
 			$post_id = $this->_registry->get( 'model.event.creating' )
 				->create_duplicate_post();
 			if ( false !== $post_id ) {
-				$created_event = $this->_registry->get( 'model.event', $post_id );
-				$this->add_exception_date(
-					$old_post_id,
-					$created_event->get( 'start' )
+				$created_event  = $this->_registry->get( 'model.event', $post_id );
+				$original_event = $this->_registry->get( 'model.event', $old_post_id );
+				$this->_handle_instances(
+					$created_event,
+					$original_event,
+					$instance_id
 				);
 				$this->_registry->get( 'model.event.instance' )->clean(
 					$old_post_id,
@@ -143,18 +151,18 @@ class Ai1ec_Event_Parent extends Ai1ec_Base {
 					1,
 					get_edit_post_link( $post_id, 'url' )
 				);
-				wp_redirect( 
+				wp_redirect(
 					apply_filters(
 						'redirect_post_location',
 						$location,
 						$post_id
-					) 
+					)
 				);
 				exit();
 			}
 		}
 	}
-	
+
 	/**
 	 * Inject base event edit link for modified instances
 	 *
@@ -196,7 +204,7 @@ class Ai1ec_Event_Parent extends Ai1ec_Base {
 		}
 		return $actions;
 	}
-	
+
 	/**
 	 * add_exception_date method
 	 *
@@ -219,5 +227,125 @@ class Ai1ec_Event_Parent extends Ai1ec_Base {
 		);
 		$event->set( 'exception_dates', implode( ',', $dates_list ) );
 		return $event->save( true );
+	}
+
+	/**
+	 * Handles instances saving and switching if needed.
+	 *
+	 * @param Ai1ec_Event $created_event  Created event object.
+	 * @param Ai1ec_Event $original_event Original event object.
+	 *
+	 * @return void Method does not return.
+	 */
+	protected function _handle_instances(
+		Ai1ec_Event $created_event,
+		Ai1ec_Event $original_event,
+		$instance_id
+	) {
+		$ce_start = $created_event->get( 'start' );
+		$oe_start = $original_event->get( 'start' );
+		if (
+			$ce_start->format() !== $oe_start->format()
+		) {
+			$this->add_exception_date(
+				$original_event->get( 'post_id' ),
+				$ce_start
+			);
+			return;
+		}
+		// find next instance
+		$next_instance = $this->_find_next_instance(
+			$original_event->get( 'post_id' ),
+			$instance_id
+		);
+		if ( ! $next_instance ) {
+			$original_event->set( 'recurrence_rules', null );
+			$original_event->save( true );
+			return;
+		}
+		$next_instances_count = $this->_count_next_instances(
+			$original_event->get( 'post_id' ),
+			$instance_id
+		);
+		$original_event->set(
+			'start',
+			$this->_registry->get( 'date.time', $next_instance->get( 'start') )
+		);
+		$original_event->set(
+			'end',
+			$this->_registry->get( 'date.time', $next_instance->get( 'end') )
+		);
+		// we need to change also number of occurencies if there is set to
+		// number not dates.
+		$recurrence_rules = $original_event->get( 'recurrence_rules' );
+		$rules_info       = $this->_registry->get( 'recurrence.rule' )
+			->build_recurrence_rules_array( $recurrence_rules );
+		if ( isset( $rules_info['COUNT'] ) ) {
+			$rules_info['COUNT'] = (int)$rules_info['COUNT'] - 1;
+			$rules               = '';
+			if ( 1 === $rules_info['COUNT'] ) {
+				$rules_info = array();
+			}
+			foreach ( $rules_info as $key=> $value ) {
+				$rules .= $key . '=' . $value . ';';
+			}
+			$original_event->set(
+				'recurrence_rules',
+				$rules
+			);
+		}
+		$original_event->save( true );
+	}
+
+	/**
+	 * Returns next instance.
+	 *
+	 * @param int $post_id     Post ID.
+	 * @param int $instance_id Instance ID/
+	 *
+	 * @return null|Ai1ec_Event Result.
+	 */
+	protected function _find_next_instance( $post_id, $instance_id ) {
+		$dbi             = $this->_registry->get( 'dbi.dbi' );
+		$table_instances = $dbi->get_table_name( 'ai1ec_event_instances' );
+		$table_posts     = $dbi->get_table_name( 'posts' );
+		$query = $dbi->prepare(
+			'SELECT i.id FROM ' . $table_instances . ' i JOIN ' .
+			$table_posts . ' p ON (p.ID = i.post_id) ' .
+			'WHERE i.post_id = %d AND i.id > %d ' .
+			'AND p.post_status = \'publish\' ' .
+			'ORDER BY id ASC LIMIT 1',
+			$post_id,
+			$instance_id
+		);
+		$next_instance_id = $dbi->get_var( $query );
+		if ( ! $next_instance_id ) {
+			return null;
+		}
+		return $this->_registry->get( 'model.search' )
+			->get_event( $post_id, $next_instance_id );
+	}
+
+	/**
+	 * Counts future instances.
+	 *
+	 * @param int $post_id     Post ID.
+	 * @param int $instance_id Instance ID/
+	 *
+	 * @return int Result.
+	 */
+	protected function _count_next_instances( $post_id, $instance_id ) {
+		$dbi             = $this->_registry->get( 'dbi.dbi' );
+		$table_instances = $dbi->get_table_name( 'ai1ec_event_instances' );
+		$table_posts     = $dbi->get_table_name( 'posts' );
+		$query = $dbi->prepare(
+			'SELECT COUNT(i.id) FROM ' . $table_instances . ' i JOIN ' .
+			$table_posts . ' p ON (p.ID = i.post_id) ' .
+			'WHERE i.post_id = %d AND i.id > %d ' .
+			'AND p.post_status = \'publish\'',
+			$post_id,
+			$instance_id
+		);
+		return (int)$dbi->get_var( $query );
 	}
 }

b95a8a10b39b16e1006e111849da7cc6b3a3a65f
lukaspawlik <lukas@time.ly>
2014-07-03 15:56:14 +0200

    Changes according to PR comments.
    AIOEC-1774

--- lib/request/redirect.php
+++ lib/request/redirect.php
@@ -15,22 +15,23 @@ class Ai1ec_Request_Redirect extends Ai1ec_Base {
 	 * Checks if current request is direct for Events cats/tags and redirects
 	 * to filtered calendar.
 	 *
-	 * @param object $wpobj WP object.
+	 * @param WP $wpobj WP object.
 	 *
 	 * @return void Method does not return.
 	 */
-	public function handle_categories_and_tags( $wpobj ) {
-		$cats   = constant( 'Ai1ec_Event_Taxonomy::CATEGORIES' );
-		$tags   = constant( 'Ai1ec_Event_Taxonomy::TAGS' );
-		$is_cat = isset( $wpobj->query_vars[$cats] );
-		$is_tag = isset( $wpobj->query_vars[$tags] );
+	public function handle_categories_and_tags( WP $wpobj ) {
+		$cats = Ai1ec_Event_Taxonomy::CATEGORIES;
+		$tags = Ai1ec_Event_Taxonomy::TAGS;
 		if (
-			! isset( $wpobj->query_vars ) ||
-			( ! $is_cat && ! $is_tag )
+			! isset( $wpobj->query_vars ) || (
+				! isset( $wpobj->query_vars[$cats] ) &&
+				! isset( $wpobj->query_vars[$tags] )
+			)
 		) {
 			return;
 		}
-
+		$is_cat = isset( $wpobj->query_vars[$cats] );
+		$is_tag = isset( $wpobj->query_vars[$tags] );
 		if ( $is_cat ) {
 			$query_ident = $cats;
 			$url_ident   = 'cat_ids';
@@ -51,13 +52,10 @@ class Ai1ec_Request_Redirect extends Ai1ec_Base {
 			'html.element.href',
 			array( $url_ident => $term->term_id )
 		);
-		$params = array(
-			'url'        => $href->generate_href(),
-			'query_args' => array(),
+		return Ai1ec_Http_Response_Helper::redirect(
+			$href->generate_href(),
+			301
 		);
-		$this->_registry->get( 'http.response.render.strategy.redirect' )
-			->render( $params );
-		Ai1ec_Http_Response_Helper::stop( 0 );
 	}
 }
 

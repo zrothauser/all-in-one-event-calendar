acb6dfc7b5d791a65f1faac404d59e7738ce4b86
lukaspawlik <lukas@time.ly>
2014-08-25 11:49:43 +0200

    Added transient to feed fetch.
    SKIPPER-16

--- lib/news/feed.php
+++ lib/news/feed.php
@@ -19,19 +19,38 @@ class Ai1ec_News_Feed extends Ai1ec_Base {
 	public function import_feed( $limit = 5 ) {
 		try {
 			include_once(
-				ABSPATH . WPINC . DIRECTORY_SEPARATOR . 'class-simplepie.php'
-			);
+					ABSPATH . WPINC . DIRECTORY_SEPARATOR . 'class-simplepie.php'
+				);
 			include_once(
 				ABSPATH . WPINC . DIRECTORY_SEPARATOR .'SimplePie'
 				. DIRECTORY_SEPARATOR .'File.php'
 			);
-			$file = new SimplePie_File( AI1EC_RSS_FEED );
-			$feed = new SimplePie();
-			$feed->set_raw_data($file->body);
-			$feed->init();
-			return is_wp_error( $feed )
-				? array()
-				: $feed->get_items( 0, $limit );
+			$result = get_transient( 'ai1ec_rss_feed' );
+			if (
+				false === $result ||
+				(
+					defined( 'AI1EC_DEBUG' ) &&
+					AI1EC_DEBUG
+				)
+			) {
+				$file = new SimplePie_File( AI1EC_RSS_FEED );
+				$feed = new SimplePie();
+				$feed->set_raw_data($file->body);
+				$feed->init();
+				$result = is_wp_error( $feed )
+					? array()
+					: $feed->get_items( 0, $limit );
+				$expiration = apply_filters(
+					'wp_feed_cache_transient_lifetime',
+					12 * HOUR_IN_SECONDS,
+					AI1EC_RSS_FEED
+				);
+				if ( empty( $result ) ) {
+					$expiration = 1 * HOUR_IN_SECONDS;
+				}
+				set_transient( 'ai1ec_rss_feed', $result, $expiration );
+			}
+			return $result;
 		} catch ( Exception $exc ) {
 			return array();
 		}

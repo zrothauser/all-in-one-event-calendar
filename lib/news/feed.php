5a203b321d8cb7e2b869fe134706ecb71e1b8671
lukaspawlik <lukas@time.ly>
2014-09-04 13:16:02 +0200

    Fixes.
    RSS

--- lib/news/feed.php
+++ lib/news/feed.php
@@ -20,14 +20,17 @@ class Ai1ec_News_Feed {
 	 * @return array RSS feed entries.
 	 */
 	public function import_feed( $limit = 5, $feed = AI1EC_RSS_FEED ) {
-        $cache  = $this->get_transient_name( $feed );
-        $result = get_transient( $cache );
+		include_once(
+			ABSPATH . WPINC . DIRECTORY_SEPARATOR . 'class-simplepie.php'
+		);
+		$cache  = $this->get_transient_name( $feed );
+		$result = get_transient( $cache );
 		if ( false === $result || AI1EC_DEBUG ) {
-            $result     = $this->fetch_feed( $limit, $feed );
-            $expiration = $this->get_expiration( $feed );
+			$result     = $this->fetch_feed( $limit, $feed );
+			$expiration = $this->get_expiration( $feed );
 			set_transient( $cache, $result, $expiration );
 		}
-        return $result;
+		return $result;
 	}
 
 	/**
@@ -38,26 +41,23 @@ class Ai1ec_News_Feed {
 	 *
 	 * @return array List of feed entries.
 	 */
-    public function fetch_feed( $feed, $limit ) {
-		include_once(
-				ABSPATH . WPINC . DIRECTORY_SEPARATOR . 'class-simplepie.php'
-			);
+	public function fetch_feed( $limit, $feed ) {
 		include_once(
 			ABSPATH . WPINC . DIRECTORY_SEPARATOR .'SimplePie'
 			. DIRECTORY_SEPARATOR .'File.php'
 		);
-        $result = array();
-        try {
-			$file = new SimplePie_File( AI1EC_RSS_FEED );
+		$result = array();
+		try {
+			$file = new SimplePie_File( $feed );
 			$feed = new SimplePie();
 			$feed->set_raw_data( $file->body );
 			$feed->init();
 			if ( ! is_wp_error( $feed ) ) {
 				$result = $feed->get_items( 0, $limit );
-            }
-        } catch ( Exception $exception ) {} // discard
-        return $result;
-    }
+			}
+		} catch ( Exception $exception ) {} // discard
+		return $result;
+	}
 
 	/**
 	 * Get name to be used for transient.
@@ -66,11 +66,11 @@ class Ai1ec_News_Feed {
 	 *
 	 * @return string Transient name to use.
 	 */
-    public function get_transient_name( $feed ) {
-        return ( AI1EC_RSS_FEED === $feed )
-            ? 'ai1ec_rss_feed' :
-            'ai1ec_rss_' . substr( md5( $feed ), 5, 8 );
-    }
+	public function get_transient_name( $feed ) {
+		return ( AI1EC_RSS_FEED === $feed )
+			? 'ai1ec_rss_feed' :
+			'ai1ec_rss_' . substr( md5( $feed ), 5, 8 );
+	}
 
 	/**
 	 * Get expiration time (in seconds) for feed.
@@ -79,16 +79,13 @@ class Ai1ec_News_Feed {
 	 *
 	 * @return int Number of seconds to keep cached feed output.
 	 */
-    public function get_expiration( $feed ) {
+	public function get_expiration( $feed ) {
 		$expiration = apply_filters(
 			'wp_feed_cache_transient_lifetime',
 			12 * HOUR_IN_SECONDS,
 			$feed
 		);
-		if ( empty( $result ) ) {
-			$expiration = 1 * HOUR_IN_SECONDS;
-		}
-        return $expiration;
-    }
+		return $expiration;
+	}
 
 }

0f08510f9602083e92819fffd4f80f7786ac69a6
uchu <pavel@time.ly>
2014-09-10 12:55:21 +0400

    AIOEC-1917 #comment Displaying add-ons page

--- app/view/admin/add-ons.php
+++ app/view/admin/add-ons.php
@@ -19,16 +19,16 @@ class Ai1ec_View_Add_Ons extends Ai1ec_View_Admin_Abstract {
 	 */
 	public function add_page() {
 		// =======================
-		// = Calendar Feeds Page =
+		// = Calendar Add Ons Page =
 		// =======================
-		$calendar_feeds = add_submenu_page(
+		$addons_list = add_submenu_page(
 			AI1EC_ADMIN_BASE_URL,
 			Ai1ec_I18n::__( 'Add Ons' ),
 			Ai1ec_I18n::__( 'Add Ons' ),
 			'manage_ai1ec_feeds',
 			AI1EC_PLUGIN_NAME . '-add-ons',
 			array( $this, 'display_page' )
-		);	
+		);
 	}
 	/**
 	 * Display Add Ons list page.
@@ -36,26 +36,32 @@ class Ai1ec_View_Add_Ons extends Ai1ec_View_Admin_Abstract {
 	 * @return void
 	 */
 	public function display_page() {
+		wp_enqueue_style(
+			'ai1ec_addons.css',
+			AI1EC_ADMIN_THEME_CSS_URL . 'addons.css',
+			array(),
+			AI1EC_VERSION
+		);
 		ob_start(); ?>
 		<div class="wrap" id="ai1ec-add-ons">
 			<h2>
 				<?php echo Ai1ec_I18n::__( 'Add Ons for All In One Event Calendar' ); ?>
-				&nbsp;&mdash;&nbsp;<a href="http://www.gettimely.com/product-add-ons" class="button-primary" title="<?php echo Ai1ec_I18n::__( 'Browse All Extensions' ); ?>" target="_blank"><?php echo Ai1ec_I18n::__( 'Browse All Extensions' ); ?></a>
+				&nbsp;&mdash;&nbsp;<a href="https://time.ly/downloads/" class="button-primary" title="<?php echo Ai1ec_I18n::__( 'Browse All Extensions' ); ?>" target="_blank"><?php echo Ai1ec_I18n::__( 'Browse All Extensions' ); ?></a>
 			</h2>
 			<p><?php echo Ai1ec_I18n::__( 'These add-ons extend the functionality of All In One Event Calendar.' ); ?></p>
 			<?php
-				if ( false === ( $cache = get_transient( 'ai1ec_add_ons_feed' ) ) ) {
-					$feed = wp_remote_get( 'https://time.ly/downloads/feed/', array( 'sslverify' => false ) );
+				if ( false === ( $cache = get_transient( 'ai1ec_addons_listxx' ) ) ) {
+					$feed = wp_remote_get( 'http://localhost/?action=addons_list', array( 'sslverify' => false ) );
 					if ( ! is_wp_error( $feed ) ) {
 						if ( isset( $feed['body'] ) && strlen( $feed['body'] ) > 0 ) {
 							$cache = wp_remote_retrieve_body( $feed );
-							set_transient( 'ai1ec_add_ons_feed', $cache, 3600 );
+							set_transient( 'ai1ec_addons_list', $cache, 3600 );
 						}
 					} else {
 						$cache = '<div class="error"><p>' . Ai1ec_I18n::__( 'There was an error retrieving the extensions list from the server. Please try again later.' ) . '</div>';
 					}
 				}
-				echo  $cache;
+				echo json_decode( $cache );
 			?>
 		</div>
 		<?php

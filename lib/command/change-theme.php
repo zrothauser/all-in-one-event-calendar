c2ab2c83e183a43dbc17d43e2662a7dafaf4ea04
Farmer Paul <read.ishmael@gmail.com>
2014-10-26 20:02:13 -0400

    STEM-5: Made theme change command callable without an HTTP request

--- lib/command/change-theme.php
+++ lib/command/change-theme.php
@@ -24,23 +24,13 @@ class Ai1ec_Command_Change_Theme extends Ai1ec_Command {
 			'',
 			$_GET['ai1ec_stylesheet']
 		);
-		update_option(
-			'ai1ec_current_theme',
-			array(
-				'theme_dir'  => realpath( $_GET['ai1ec_theme_dir'] ),
-				'theme_root' => realpath( $_GET['ai1ec_theme_root'] ),
-				'theme_url'  => $_GET['ai1ec_theme_url'],
-				'legacy'     => (bool)intval( $_GET['ai1ec_legacy'] ),
-				'stylesheet' => $stylesheet,
-			)
+		$this->switch_theme(
+			realpath( $_GET['ai1ec_theme_root'] ),
+			realpath( $_GET['ai1ec_theme_dir'] ),
+			$_GET['ai1ec_theme_url'],
+			$stylesheet,
+			(bool)intval( $_GET['ai1ec_legacy'] )
 		);
-		// Delete user variables from database so fresh ones are used by new theme.
-		$this->_registry->get( 'model.option' )->delete(
-			Ai1ec_Less_Lessphp::DB_KEY_FOR_LESS_VARIABLES
-		);
-		// Recompile CSS for new theme.
-		$css_controller = $this->_registry->get( 'css.frontend' );
-		$css_controller->invalidate_cache( null, false );
 
 		// Return user to themes list page with success message.
 		return array(
@@ -81,4 +71,32 @@ class Ai1ec_Command_Change_Theme extends Ai1ec_Command {
 		return false;
 	}
 
+	/**
+	 * Switch to the given calendar theme.
+	 *
+	 * @param  string $theme_root The theme's parent directory
+	 * @param  string $theme_dir  The theme's directory
+	 * @param  string $theme_url  The URL to the theme's resources
+	 * @param  string $stylesheet The name of the theme
+	 * @param  bool   $legacy     Whether this is a legacy theme
+	 */
+	public function switch_theme(
+		$theme_root,
+		$theme_dir,
+		$theme_url,
+		$stylesheet,
+		$legacy
+	) {
+		update_option(
+			'ai1ec_current_theme',
+			compact( 'theme_dir', 'theme_root', 'theme_url', 'legacy', 'stylesheet' )
+		);
+		// Delete user variables from database so fresh ones are used by new theme.
+		$this->_registry->get( 'model.option' )->delete(
+			Ai1ec_Less_Lessphp::DB_KEY_FOR_LESS_VARIABLES
+		);
+		// Recompile CSS for new theme.
+		$css_controller = $this->_registry->get( 'css.frontend' );
+		$css_controller->invalidate_cache( null, false );
+	}
 }

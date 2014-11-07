dec83f7616c5f72b6f77c9a9a0ba2442b64f10f4
Lukas Pawlik <lukas@time.ly>
2014-11-07 12:07:54 +0100

    Moved apply_filters check into metabox method handler instead of calendar init method (due to load order).
    SQRT-4

--- app/view/admin/add-new-event.php
+++ app/view/admin/add-new-event.php
@@ -39,6 +39,9 @@ class Ai1ec_View_Add_New_Event extends Ai1ec_Base {
 	 * @return void
 	 */
 	public function event_banner_meta_box_container() {
+		if ( ! apply_filters( 'ai1ec_use_banner_image', false ) ) {
+			return;
+		}
 		add_meta_box(
 			AI1EC_POST_TYPE. '_banner',
 			Ai1ec_I18n::__( 'Banner Image' ),

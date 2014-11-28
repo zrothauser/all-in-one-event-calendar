d39c2b49ff9ac70b8b5fd188d949d785c64378b5
uchu <pavel@time.ly>
2014-11-28 20:23:09 +0530

    BIGFOOT-1 #comment Initial plugin setting up

--- app/view/calendar/view/agenda.php
+++ app/view/calendar/view/agenda.php
@@ -106,6 +106,11 @@ class Ai1ec_Calendar_View_Agenda extends Ai1ec_Calendar_View_Abstract {
 				'no_navigation'    => $view_args['no_navigation'],
 				'pagination_links' => $pagination_links,
 				'views_dropdown'   => $view_args['views_dropdown'],
+				'big_filtering'    => apply_filters(
+					'ai1ec_big_filtering',
+					'',
+					$type
+				),
 			);
 			// Add extra buttons to Agenda view's nav bar if events were returned.
 			if ( $type === 'agenda' && $dates ) {

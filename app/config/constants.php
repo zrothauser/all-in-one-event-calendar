8d9f50824904940501b5a15f9ffba97678baac0b
uchu <pavel@time.ly>
2014-12-16 17:37:54 +0530

    AIOEC-2118 #comment Wrongly commited constants.php

--- app/config/constants.php
+++ app/config/constants.php
@@ -455,7 +455,7 @@ function ai1ec_initiate_constants( $ai1ec_base_dir, $ai1ec_base_url ) {
 	// you know what you are doing, because you will waste valuable
 	// resources - save the Earth, at least.
 	if ( ! defined( 'AI1EC_DEBUG' ) ) {
-		define( 'AI1EC_DEBUG', true );
+		define( 'AI1EC_DEBUG', false );
 	}
 
 	// Enable Ai1EC cache functionality. If you set this to false, only cache
@@ -463,7 +463,7 @@ function ai1ec_initiate_constants( $ai1ec_base_dir, $ai1ec_base_url ) {
 	// This is pointless in any case other than development, where literary
 	// every second refresh needs to take fresh copy of everything.
 	if ( ! defined( 'AI1EC_CACHE' ) ) {
-		define( 'AI1EC_CACHE', false );
+		define( 'AI1EC_CACHE', true );
 	}
 
 	if ( ! defined( 'AI1EC_DISABLE_FILE_CACHE' ) ) {

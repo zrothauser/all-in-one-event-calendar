fa9493cad1dfcf211834b8a4d6c34e3903dcc240
Lukas Pawlik <lukas@time.ly>
2014-12-19 14:18:33 +0100

    Removed filters responsible for adding microformats html tags.
    AIOEC-2125

--- lib/http/response/render/strategy/html.php
+++ lib/http/response/render/strategy/html.php
@@ -21,7 +21,11 @@ class Ai1ec_Render_Strategy_Html extends Ai1ec_Http_Response_Render_Strategy {
 	protected $_html;
 
 	/**
-	 * @var string The html for the footer of the event.
+	 * Registers proper filters for content modifications.
+	 *
+	 * @param array $params Function params.
+	 *
+	 * @return void Method does not return.
 	 */
 	protected $_html_footer = '';
 
@@ -44,8 +48,6 @@ class Ai1ec_Render_Strategy_Html extends Ai1ec_Http_Response_Render_Strategy {
 		if ( isset( $params['is_event'] ) ) {
 			// Filter event post content, in single- and multi-post views
 			add_filter( 'the_content', array( $this, 'event_content' ), PHP_INT_MAX - 1 );
-			add_filter( 'the_title',   array( $this, 'event_title' ), PHP_INT_MAX - 1, 3 );
-			add_filter( 'post_class',  array( $this, 'post_class' ), PHP_INT_MAX - 1, 3 );
 			return;
 		}
 		// Replace page content - make sure it happens at (almost) the very end of
@@ -97,58 +99,12 @@ class Ai1ec_Render_Strategy_Html extends Ai1ec_Http_Response_Render_Strategy {
 	 * @return string         Post/Page content
 	 **/
 	public function event_content( $content ) {
-
-		// if we have modified the content, we return the modified version.
-		$to_return = $this->_html .
-			'<div class="p-description">' . $content . '</div>';
-		if ( isset( $this->_html_footer ) ) {
-			$to_return .= $this->_html_footer;
-		}
 		// Pass the orginal content to the filter so that it can be modified
 		return apply_filters(
 			'ai1ec_event_content',
-			$to_return,
+			$this->_html . $content,
 			$content
 		);
-	}
-
-	/**
-	 * Add microformats class to title
-	 *
-	 * @param string $title
-	 * @param id     $post_id
-	 * @param bool   $is_view is the filter called from a calendar view?
-	 *               In that case do not render the extra markup
-	 */
-	public function event_title( $title, $post_id, $is_view = false ) {
-		if (
-			false === $is_view &&
-			true  === $this->_registry->get( 'acl.aco' )
-				->is_our_post_type( $post_id )
-		) {
-			$title = '<span class="p-summary">' . $title . '</span>';
-		}
-		return $title;
-	}
-
-	/**
-	 * Add vevent class to post
-	 *
-	 * @param array  $classes
-	 * @param string $class   A comma-separated list of additional classes added
-	 *                        to the post.
-	 * @param int    $post_id The post ID.
-	 *
-	 * @return array
-	 */
-	public function post_class( $classes, $class, $post_id ) {
-		if (
-			true === $this->_registry->get( 'acl.aco' )
-				->is_our_post_type( $post_id )
-		) {
-			$classes[] = 'h-event';
-		}
-		return $classes;
 	}
 
 }

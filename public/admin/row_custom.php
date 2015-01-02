20f067699fcb1157ed542ebf6664c879af4259d5
uchu <pavel@time.ly>
2015-01-02 17:16:06 +0530

    AIOEC-715 Custom reccurence dates.

---
+++ public/admin/row_custom.php
@@ -0,0 +1,11 @@
+<div class="ai1ec-form-group">
+	<label class="ai1ec-control-label ai1ec-col-sm-3">
+		Custom dates:
+	</label>
+	<div class="ai1ec-col-sm-9">
+		<div id="ai1ec_recurrence_calendar"></div>
+		<div id="ai1ec_rec_dates_list"></div>
+		<input type="hidden" name="ai1ec_rec_custom_dates"
+			   id="ai1ec_rec_custom_dates" value="">
+	</div>
+</div>

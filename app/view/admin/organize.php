ce62233008aeebda822ebb565d99430d642d090b
Nicola Peluchetti <nicola.peluchetti@gmail.com>
2015-01-07 20:58:53 +0100

    took more out of core

--- app/view/admin/organize.php
+++ app/view/admin/organize.php
@@ -22,27 +22,23 @@ class Ai1ec_View_Organize extends Ai1ec_Base {
 	public function add_taxonomy_actions() {
 		$taxonomies = get_object_taxonomies( AI1EC_POST_TYPE, 'object' );
 		$dispatcher = $this->_registry->get( 'event.dispatcher' );
-		$customize_groups = apply_filters( 'ai1ec_add_custom_groups', array() );
+		$taxonomy_metadata = array(
+			'events_categories' => array(
+				'icon' => 'ai1eccfgi-folder'
+			),
+			'events_tags'       => array(
+				'icon' => 'ai1eccfgi-tag'
+			)
+		);
+		$taxonomy_metadata = apply_filters( 'ai1ec_add_custom_groups', $taxonomy_metadata );
 		$group_data = array();
-		foreach ( $customize_groups as $group ) {
-			$group_data[$group['taxonomy_name']] = array(
-				'icon' => $group['icon'],
-				'id'   => $group['id'],
-			);
-		}
 		do_action( 'ai1ec_taxonomy_management_css' );
 		foreach ( $taxonomies as $taxonomy => $data ) {
 			if ( true === $data->public ) {
 				$active_taxonomy = isset( $_GET['taxonomy'] ) && $taxonomy === $_GET['taxonomy'];
 				$edit_url = '';
-				if ( true === $active_taxonomy && isset( $icons[$taxonomy] ) ) {
-					$edit_url = add_query_arg(
-						array(
-							'action' => 'edit',
-							'id'     => $group_data[$taxonomy]['id']
-					 	),
-						$this->_registry->get( 'view.admin.custom-filters' )->get_url()
-					);
+				if ( true === $active_taxonomy && isset( $taxonomy_metadata[$taxonomy]['url'] ) ) {
+					$edit_url = $taxonomy_metadata[$taxonomy]['url'];
 				}
 				$this->_taxonomies[] = array(
 					'url'        => add_query_arg(
@@ -54,8 +50,8 @@ class Ai1ec_View_Organize extends Ai1ec_Base {
 					),
 					'name'       => $data->labels->name,
 					'active'     => $active_taxonomy,
-					'icon'       => isset( $group_data[$taxonomy] ) ?
-						$group_data[$taxonomy]['icon'] :
+					'icon'       => isset( $taxonomy_metadata[$taxonomy] ) ?
+						$taxonomy_metadata[$taxonomy]['icon'] :
 						'',
 					'edit_url'   => $edit_url,
 					'edit_label' => Ai1ec_I18n::__( 'Edit' )

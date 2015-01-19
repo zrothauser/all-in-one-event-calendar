ce4a69c14795d7f3f403ba82f81e3e536048b91c
Lukas Pawlik <lukas@time.ly>
2015-01-19 11:18:06 +0100

    Added calendar state class for SKIPPER-66 purposes.
    SKIPPER-66

---
+++ lib/calendar/state.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Calendar state container;
+ *
+ * @author     Time.ly Network Inc.
+ * @since      2.2
+ *
+ * @package    AI1EC
+ * @subpackage AI1EC.Lib.Calendar
+ */
+class Ai1ec_Calendar_State extends Ai1ec_Base {
+
+	/**
+	 * Whether calendar is initializing router or not.
+	 *
+	 * @var bool
+	 */
+	public $init_router = false;
+}

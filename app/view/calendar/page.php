c00a541d2067d616e732499f703a405ac726fe8f
uchu <pavel@time.ly>
2015-03-19 00:56:55 +0530

    (#2) Changes for SAS plugin

--- app/view/calendar/page.php
+++ app/view/calendar/page.php
@@ -163,6 +163,10 @@ class Ai1ec_Calendar_Page extends Ai1ec_Base {
 					$type,
 					$caller
 				),
+				'additional_buttons'           => apply_filters(
+					'ai1ec_additional_buttons',
+					''
+				),
 				'show_dropdowns'               => apply_filters(
 					'ai1ec_show_dropdowns',
 					true
@@ -208,6 +212,14 @@ class Ai1ec_Calendar_Page extends Ai1ec_Base {
 				),
 				'inline_js_calendar'           => apply_filters(
 					'ai1ec_inline_js_calendar',
+					''
+				),
+				'after_view'                   => apply_filters(
+					'ai1ec_after_view',
+					''
+				),
+				'ai1ec_above_calendar'         => apply_filters(
+					'ai1ec_above_calendar',
 					''
 				),
 			);

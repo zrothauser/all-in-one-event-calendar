1a7b50a0010e79c90e146fa93e1f7ecf722848d8
Calvin Yeh <calvinyeh@microntec.com.br>
2015-08-31 15:09:22 -0700

    PRD-15: Removing static calls

--- lib/upload/size-determiner.php
+++ lib/upload/size-determiner.php
@@ -17,8 +17,10 @@ class Ai1ec_Upload_Size_Determiner_Utility {
 	 *
 	 * @return int Maximum upload file size
 	 */
-	public static function get_maximum_upload_file_size_bytes( $size ) {
-		$maximum_size = Ai1ec_Size_Converter_Utility::convert_hr_to_bytes( $size );
+	public function get_maximum_upload_file_size_bytes( $size ) {
+		$converter_utility = new Ai1ec_Size_Converter_Utility();
+
+		$maximum_size = $converter_utility->convert_hr_to_bytes( $size );
 
 		if ( $maximum_size <= 0  ||  $maximum_size > wp_max_upload_size() ) {
 			$maximum_size = wp_max_upload_size();
@@ -34,8 +36,8 @@ class Ai1ec_Upload_Size_Determiner_Utility {
 	 *
 	 * @return string Human readable maximum upload file size
 	 */
-	public static function get_maximum_upload_file_size_string( $size ) {
-		$size_string = size_format( self::get_maximum_upload_file_size_bytes( $size ), 2 );
+	public function get_maximum_upload_file_size_string( $size ) {
+		$size_string = size_format( $this->get_maximum_upload_file_size_bytes( $size ), 2 );
 
 		return $size_string;
 	}

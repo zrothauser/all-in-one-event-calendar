f17e941f2d9ef27a25ed3d1756872225e401479e
uchu <pavel@time.ly>
2015-12-11 19:10:02 +0530

    PROD-541: Commit after solve conflict

--- public/js_src/scripts/event/gmaps_helper.js
+++ public/js_src/scripts/event/gmaps_helper.js
@@ -5,16 +5,25 @@ define(
 		 function( $ ) {
 	'use strict'; // jshint ;_;
 	var init_gmaps = function() {
-		var options = {
+		var
+			lat_lng       = $( '#ai1ec-gmap-address' )[0].value.split( ',' ),
+			lat           = parseFloat( lat_lng[0] ),
+			lng           = parseFloat( lat_lng[1] ),
+			gmaps_lat_lng = new google.maps.LatLng( lat, lng ),
+			options = {
 				zoom        : 14,
 				mapTypeId   : google.maps.MapTypeId.ROADMAP,
 				scrollwheel : false
-			};
-			var
-				map = new google.maps.Map( $( '#ai1ec-gmap-canvas' )[0], options ),
-				marker = new google.maps.Marker( { map: map } ),
-				geocoder = new google.maps.Geocoder();
+			},
+			map           = new google.maps.Map( $( '#ai1ec-gmap-canvas' )[0], options ),
+			marker        = new google.maps.Marker( { map: map } ),
+			geocoder      = new google.maps.Geocoder();
 
+		if ( lat && lng && gmaps_lat_lng ) {
+			map.setCenter( gmaps_lat_lng );
+			marker.setPosition( gmaps_lat_lng );
+		} else {
+			// If lat and lng were not sucessfully parsed, try searching.
 			geocoder.geocode(
 				{
 					'address': $( '#ai1ec-gmap-address' )[0].value
@@ -26,6 +35,7 @@ define(
 					}
 				}
 			);
+		}
 	};
 	var handle_show_map_when_clicking_on_placeholder = function() {
 		var map_el = $( '.ai1ec-gmap-container-hidden:first' );

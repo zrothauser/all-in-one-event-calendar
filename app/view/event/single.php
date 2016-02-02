4cde4b20f0adab44cdfe84040a8411c16d24c17e
Eli Barbosa <elirenato2000@gmail.com>
2016-02-02 15:30:32 -0200

    Organizing API class to have more cohesion

--- app/view/event/single.php
+++ app/view/event/single.php
@@ -155,18 +155,18 @@ class Ai1ec_View_Event_Single extends Ai1ec_Base {
 		if ( AI1EC_API && AI1EC_API_TICKETING ) { 
 			$api_event_id = get_post_meta(
 				$event->get( 'post_id' ),
-				Ai1ec_Api::EVENT_ID_METADATA,
+				Ai1ec_Api_Ticketing::EVENT_ID_METADATA,
 				true
 			);
 			if ( $api_event_id ) {
-				$api                  = $this->_registry->get( 'model.api' );
+				$api                  = $this->_registry->get( 'model.api.api-ticketing' );
 				$args['api_event_id'] = $api_event_id;
 				$checkout_url         = null;
 				if ( false === ai1ec_is_blank( $event->get( 'ical_feed_url' ) ) ) {
 					//if this ticket event is imported, uses the api and checkout url from the imported information
 					$checkout_url = get_post_meta(
 						$event->get( 'post_id' ),
-						Ai1ec_Api::ICS_CHECKOUT_URL_METADATA,
+						Ai1ec_Api_Ticketing::ICS_CHECKOUT_URL_METADATA,
 						true
 					);
 				}

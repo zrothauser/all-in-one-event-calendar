c96ef4f4604b873b288c8d2d6aeb701b52b8fc8b
Eli Barbosa <elirenato2000@gmail.com>
2016-02-17 11:00:35 -0200

    First configuration to run phpunit inside core plugin

---
+++ tests/test-case.php
@@ -0,0 +1,88 @@
+<?php
+
+class BaseTestCase extends WP_UnitTestCase {
+	
+	public function setUp(){
+		parent::setUp();		
+	}
+
+	public function tearDown(){
+		parent::tearDown();				
+	}
+
+	/**
+	 * Sign in into the Wordpress
+	 */
+	protected function wp_sign() {		
+		$current_user = wp_get_current_user();
+		if ( $current_user->ID == 0 ) {
+			$user = wp_signon( [ 'user_login' => 'admin', 'user_password' => 'password'] );
+			if ( ! $user instanceof WP_User ) {
+				throw new Exception( 'Authentication error' );			
+			}
+			wp_set_current_user( $user->ID );							
+		} 
+	}
+
+	/**
+	 * Sign in to Timely Network
+	 */
+	protected function api_sign() {
+		global $ai1ec_registry;
+		$_POST['ai1ec_email']    = 'eli@time.ly';
+		$_POST['ai1ec_password'] = '123456';
+		$api                     = $ai1ec_registry->get( 'model.api.api-registration' );
+		if ( false === $api->is_signed() ) {
+			$this->assertTrue( $api->signin(), 'Authentication error. Check the user name and password to access the API' );
+		} 		
+	}	
+
+	/**
+	 * Create the _POST variable used internaly by a lot of methods
+	 */
+	protected function create_post_request( $data ) {
+		$_SERVER['REQUEST_METHOD']   = 'POST';
+		$_REQUEST = $_POST = $GLOBALS['_POST'] = $data;
+	}
+
+	/**
+	 * Create the _POST variable used internaly by a lot of methods
+	 */
+	protected function create_get_request( $data ) {
+		$_SERVER['REQUEST_METHOD']   = 'GET';
+		$_REQUEST = $_POST = $GLOBALS['_GET'] = $data;
+	}
+
+	/**
+	 * Assert if a value is not blank
+	 */
+	protected function assertNotBlank( $value, $message = '' ) {
+		if ( ai1ec_is_blank( $value ) ) {
+			$this->fail( $message );
+		}
+	}
+
+	/**
+	 * Assert if the array is not empty
+	 */
+	protected function assertArrayNotEmpty( $value, $message = '' ) {
+		if ( is_null( $value ) || 
+			! is_array( $value ) || 
+			0 === count( $value ) ) {
+			$this->fail( $message );
+		}
+	}
+
+	/**
+	 * Assert the size of the array has the size expected
+	 */
+	protected function assertArrayCount( $value, $expected, $message = '' ) {
+		if ( is_null( $value ) || 
+			! is_array( $value ) || 
+			$expected !== count( $value ) ) {
+			$this->fail( $message );
+		}
+	}
+
+}
+

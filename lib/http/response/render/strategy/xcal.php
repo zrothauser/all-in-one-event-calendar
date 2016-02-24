d4dc08ac14e75ac22684cc7af134f5ad4111d0f8
uchu <pavel@time.ly>
2016-02-24 15:22:29 +0530

    (PROD-943) Added xcal strategy file

--- lib/http/response/render/strategy/xcal.php
+++ lib/http/response/render/strategy/xcal.php
@@ -4,7 +4,7 @@
  * Render the request as xcal.
  *
  * @author     Time.ly Network Inc.
- * @since      2.0
+ * @since      2.3
  *
  * @package    AI1EC
  * @subpackage AI1EC.Http.Response.Render.Strategy
@@ -16,13 +16,13 @@ class Ai1ec_Render_Strategy_Xcal extends Ai1ec_Http_Response_Render_Strategy {
 	*/
 	public function render( array $params ) {
 		$this->_dump_buffers();
-		header("Content-Type: application/force-download; name=\"calendar.xml");
-		header("Content-type: text/xml"); 
-		header("Content-Transfer-Encoding: binary");
-		header("Content-Disposition: attachment; filename=\"calendar.xml");
-		header("Expires: 0");
-		header("Cache-Control: no-cache, must-revalidate");
-		header("Pragma: no-cache");
+		header( 'Content-Type: application/force-download; name="calendar.xml"' );
+		header( 'Content-type: text/xml' ); 
+		header( 'Content-Transfer-Encoding: binary' );
+		header( 'Content-Disposition: attachment; filename="calendar.xml"' );
+		header( 'Expires: 0' );
+		header( 'Cache-Control: no-cache, must-revalidate' );
+		header( 'Pragma: no-cache' );
 		echo $params['data'];
 		return Ai1ec_Http_Response_Helper::stop( 0 );
 	}

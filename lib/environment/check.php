261002d7a0273c94a569bf46784da717c417c8bd
uchu <pavel@time.ly>
2016-03-18 20:18:08 +0530

    (PROD-1260) Dismissable warning

--- lib/environment/check.php
+++ lib/environment/check.php
@@ -96,29 +96,38 @@ class Ai1ec_Environment_Checks extends Ai1ec_Base {
 			return;
 		}
 		// Check for needed PHP extensions.
-		if ( ! function_exists( 'iconv' ) ) {
+		$option  = $this->_registry->get( 'model.option' );
+		if (
+			! function_exists( 'iconv' ) &&
+			! $option->get( 'ai1ec_iconv_notification' )
+		) {
 			$msg = Ai1ec_I18n::__(
 					'PHP extension "iconv" needed for All-In-One-Event-Calendar is missing. Please, check your PHP configuration.<br />'
 				);
 			$notification->store(
 				$msg,
 				'error',
-				2,
+				0,
 				array( Ai1ec_Notification_Admin::RCPT_ADMIN ),
 				true
 			);
+			$option->set( 'ai1ec_iconv_notification', true );
 		}
-		if ( ! function_exists( 'mb_check_encoding' ) ) {
+		if (
+			! function_exists( 'mb_check_encoding' ) &&
+			! $option->get( 'ai1ec_mbstring_notification' )
+		) {
 			$msg = Ai1ec_I18n::__(
 					'PHP extension "mbstring" needed for All-In-One-Event-Calendar is missing. Please, check your PHP configuration.<br />'
 				);
 			$notification->store(
 				$msg,
 				'error',
-				2,
+				0,
 				array( Ai1ec_Notification_Admin::RCPT_ADMIN ),
 				true
 			);
+			$option->set( 'ai1ec_mbstring_notification', true );
 		}
 		global $wp_rewrite;
 		$option  = $this->_registry->get( 'model.option' );

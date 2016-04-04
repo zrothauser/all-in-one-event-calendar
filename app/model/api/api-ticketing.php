380ae70dac6cf4e091b75d2e97716b78a1b9971b
Eli Barbosa <elirenato2000@gmail.com>
2016-04-04 16:39:33 -0300

    Fix ticketing on/off feature

--- app/model/api/api-ticketing.php
+++ app/model/api/api-ticketing.php
@@ -254,83 +254,6 @@ class Ai1ec_Api_Ticketing extends Ai1ec_Api_Abstract {
 	}
 
 	/**
-	 * @return object Response body from API.
-	 */
-	public function save_payment_preferences() {
-		$calendar_id = $this->_get_ticket_calendar();
-		if ( 0 >= $calendar_id ) {
-			return false;
-		}
-		$settings  = array(
-			'payment_method' => $_POST['ai1ec_payment_method'],
-			'paypal_email'   => $_POST['ai1ec_paypal_email'],
-			'first_name'     => $_POST['ai1ec_first_name'],
-			'last_name'      => $_POST['ai1ec_last_name'],
-			'currency'       => $_POST['ai1ec_currency']
-		);
-		$custom_headers['content-type'] = 'application/x-www-form-urlencoded';
-		$response = $this->request_api( 'PUT', AI1EC_API_URL . 'calendars/' . $calendar_id . '/payment', 
-			$settings, 
-			true, //decode response body
-			$custom_headers 
-		);
-		if ( $this->is_response_success( $response ) ) {
-			$this->save_payment_settings( $settings );
-			$notification  = $this->_registry->get( 'notification.admin' );
-			$notification->store( 
-				__( 'Payment preferences were saved.', AI1EC_PLUGIN_NAME ), 
-				'updated', 
-				0, 
-				array( Ai1ec_Notification_Admin::RCPT_ADMIN ), 
-				false 
-			);
-			return $response->body;
-		} else {
-			$this->save_error_notification( $response, 
-				__( 'Payment preferences were not saved.', AI1EC_PLUGIN_NAME )
-			);
-			return false;
-		}
-	}
-
-	/**
-	 * @return object Response from API, or empty defaults
-	 */
-	public function get_payment_preferences() {
-		$calendar_id = $this->_get_ticket_calendar();
-		$settings    = null;
-		if ( 0 < $calendar_id ) {
-			$response = $this->request_api( 'GET', AI1EC_API_URL . "calendars/$calendar_id/payment", 
-				null, //no body 
-				true //decode response body
-			);
-			if ( $this->is_response_success( $response ) ) {
-				$settings = $response->body;
-			}
-		}
-		if ( is_null( $settings ) ) {
-			return (object) array( 'payment_method'=>'paypal', 'paypal_email'=> '', 'first_name'=>'',  'last_name'=>'', 'currency'=> 'USD' );
-		} else {
-			if ( ! isset( $settings->currency ) ) {
-				$settings->currency = 'USD';
-			}
-			return $settings;	
-		}		
-	}
-
-	/**
-     * Check if the current WP instance has payments settings configured
-     */
-    public function has_payment_settings() {
-    	$payment_settings = $this->get_payment_settings();
-    	if ( null === $payment_settings ) {
-    		$payment_settings = $this->get_payment_preferences();
-    		$this->save_payment_settings( (array) $payment_settings );
-    	}    
-    	return parent::has_payment_settings();
-    }
-
-	/**
 	 * Parse the fields of an Event to the structure used by API
 	 */
 	protected function _parse_event_fields_to_api_structure( Ai1ec_Event $event , WP_Post $post, $post_ticket_types, $api_fields_values  ) {
@@ -572,42 +495,6 @@ class Ai1ec_Api_Ticketing extends Ai1ec_Api_Abstract {
 				true 
 			);
 			return json_encode( array( 'data' => array(), 'error' => $error_message ) );
-		}
-	}
-	
-	public function _order_comparator( $order1, $order2 ) {
-		return strcmp( $order1->created_at, $order2->created_at ) * -1;
-	}
-
-	/**
-	 * @return object Response body in JSON.
-	 */
-	public function get_purchases() {
-		$request  = array(
-			'headers' => $this->_get_headers(),
-			'timeout' => parent::DEFAULT_TIMEOUT
-			);
-		$url           = AI1EC_API_URL . 'calendars/' . $this->_get_ticket_calendar() . '/sales';
-		$response      = wp_remote_get( $url, $request );
-		$response_code = wp_remote_retrieve_response_code( $response );
-		if ( 200 === $response_code ) {
-			$result = json_decode( $response['body'] );
-			if ( isset( $result->orders ) ) {
-				usort( $result->orders, array( "Ai1ec_Api_Ticketing", "_order_comparator" ) );
-				return $result->orders;
-			} else {
-				return array();
-			}
-		} else {
-			$error_message = $this->_transform_error_message( 
-				__( 'We were unable to get the Sales information from Time.ly Ticketing', AI1EC_PLUGIN_NAME ), 
-				$response, 
-				$url, 
-				true 
-			);
-			$notification = $this->_registry->get( 'notification.admin' );
-			$notification->store( $error_message, 'error', 0, array( Ai1ec_Notification_Admin::RCPT_ADMIN ), false );
-			return array();
 		}
 	}
 

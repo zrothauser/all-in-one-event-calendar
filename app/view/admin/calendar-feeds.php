ad1ab4c1278190a22ec531beed27d7af3f195cc7
uchu <pavel@time.ly>
2016-04-27 16:06:41 +0300

    (PROD-1452) Fixed some message and logics for feeds tabs

--- app/view/admin/calendar-feeds.php
+++ app/view/admin/calendar-feeds.php
@@ -84,13 +84,13 @@ class Ai1ec_View_Calendar_Feeds extends Ai1ec_View_Admin_Abstract {
 		$feeds          = array();
 
 		// Check for user subscription - Import feeds
-		if ($this->_api_registration->has_subscription_active( 'import-feeds' ) ) {
+		//if ($this->_api_registration->has_subscription_active( 'import-feeds' ) ) {
 		    array_push( $feeds, $this->_registry->get( 'calendar-feed.import' ) );
-		}
+		//}
 		// Check for user subscription - Discover events
-		if ($this->_api_registration->has_subscription_active( 'discover-events' ) ) {
+		//if ($this->_api_registration->has_subscription_active( 'discover-events' ) ) {
 		    array_push( $feeds, $this->_registry->get( 'calendar-feed.suggested' ) );
-		}
+		//}
 
 		// Add ICS
 		array_push( $feeds, $this->_registry->get( 'calendar-feed.ics' ) );

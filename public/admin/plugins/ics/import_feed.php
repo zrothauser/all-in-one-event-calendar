37109c144deb5939ebcb41d4a62449168532aa8d
Calvin Yeh <calvinyeh@microntec.com.br>
2016-05-05 20:43:44 -0300

    PROD-1487-2: Adding new status, when user doesn't have the feature enabled

--- public/admin/plugins/ics/import_feed.php
+++ public/admin/plugins/ics/import_feed.php
@@ -16,7 +16,7 @@
 <div class="ai1ec-feed-container ai1ec-well ai1ec-well-sm ai1ec-clearfix">
 	<?php _e(
 		'<b>You have don\'t have the subscription for this feature</b><br>
-		Please <a href="https://time.ly/pricing">sign up here</a> to a plan that
+		Please <a href="https://time.ly/pricing">upgrade here</a> to a plan that
 		supports import feeds.', AI1EC_PLUGIN_NAME )
 	?>
 </div>

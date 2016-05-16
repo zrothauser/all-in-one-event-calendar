338e69845097026a3408fe237bc513321f100976
uchu <pavel@time.ly>
2016-05-16 23:48:01 +0300

    (PROD-1558) Message about my feeds tab

--- public/admin/plugins/suggested/display_feeds.php
+++ public/admin/plugins/suggested/display_feeds.php
@@ -39,7 +39,7 @@
 			<span class="ai1ec-suggested-results-found">0</span>
 			<?php _e( ' events found.', AI1EC_PLUGIN_NAME ); ?>
 		</b>
-		<?php _e( 'Update settings for imported events in “My feeds” tab.', AI1EC_PLUGIN_NAME ); ?>
+		<?php _e( 'Update settings for the imported events in “My feeds” tab.', AI1EC_PLUGIN_NAME ); ?>
 	</div>
 	<div class="ai1ec-suggested-view-selector"> 
 		<a href="#" data-ai1ec-view="map">

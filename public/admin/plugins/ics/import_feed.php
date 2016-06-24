48b7ee5010a87a31227337e9364f102ec32181ca
Calvin Yeh <calvinyeh@microntec.com.br>
2016-06-24 18:02:43 -0300

    Inverting date/time conversion logic

--- public/admin/plugins/ics/import_feed.php
+++ public/admin/plugins/ics/import_feed.php
@@ -88,8 +88,8 @@
 		<div class="ai1ec-feed-import-timezone">
 			<label for="ai1ec_feed_import_timezone">
 				<input type="checkbox" name="ai1ec_feed_import_timezones"
-					id="ai1ec_feed_import_timezone" value="1" checked="checked">
-				<?php _e( 'Show date/time based on event\'s timezone', AI1EC_PLUGIN_NAME ); ?>
+					id="ai1ec_feed_import_timezone" value="1">
+				<?php _e( 'Convert event\'s date/time to calendar\'s timezone', AI1EC_PLUGIN_NAME ); ?>
 			</label>
 		</div>
 	</div>
